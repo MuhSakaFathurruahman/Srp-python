c4d81325ffc31f892e9b9655834b610b34ec32a0
MuhsakaFathurrahman <msakafathurrahman@gmail.com>
2022-05-23 20:46:51 +0700

    fathur_revisi

--- latihan_srp_balok/balok.py
+++ latihan_srp_balok/balok.py
@@ -5,13 +5,13 @@ class Balok:
         self.__lebar = lebar
         self.__tinggi = tinggi
         
-    def set_panjang(self, panjang:float):
+    def set_panjang(self, panjang:float) -> None:
         self.__panjang = panjang
         
-    def set_lebar(self, lebar:float):
+    def set_lebar(self, lebar:float) -> None:
         self.__lebar = lebar
         
-    def set_tinggi(self, tinggi:float):
+    def set_tinggi(self, tinggi:float) -> None:
         self.__tinggi = tinggi
         
     def get_panjang(self) -> float:
--- latihan_srp_balok/balok_controller.py
+++ latihan_srp_balok/balok_controller.py
@@ -3,7 +3,7 @@ from balok import Balok
 class BalokController:
     
     def hitung_luas(self, balok: Balok)->float :
-        return (balok.get_panjang()*balok.get_lebar()+ balok.get_panjang()*balok.get_tinggi() + balok.get_lebar()*balok.tinggi())
+        return (2*(balok.get_panjang()*balok.get_lebar()+ balok.get_panjang()*balok.get_tinggi() + balok.get_lebar()*balok.tinggi()))
     
     def hitung_volume(self, balok: Balok)->float :
         return (balok.get_panjang * balok.get_lebar * balok.get_tinggi())
