765e65a47721cd240650554f25b6c13557138ea2
malikibrahim10203 <malikibr4h1@gmail.com>
2022-05-23 20:16:53 +0700

    MalikBalokController

---
+++ latihan_srp_balok/balok_controller.py
@@ -0,0 +1,14 @@
+from balok import Balok
+
+class BalokController:
+    
+    def hitung_luas(self, balok: Balok)->float :
+        return (balok.get_panjang()*balok.get_lebar()+ balok.get_panjang()*balok.get_tinggi() + balok.get_lebar()*balok.tinggi())
+    
+    def hitung_volume(self, balok: Balok)->float :
+        return (balok.get_panjang * balok.get_lebar * balok.get_tinggi())
+    
+    def hitung_keliling(self, balok: Balok)->float :
+        return 4 * (balok.get_panjang() + balok.get_lebar() + balok.get_tinggi())
+    
+    
