4f497155a5f4b8c291bb145e14d0b6febffe22df
MuhsakaFathurrahman <msakafathurrahman@gmail.com>
2022-05-23 20:28:21 +0700

    balok_view_Fathur

---
+++ latihan_srp_balok/balok_view.py
@@ -0,0 +1,10 @@
+from balok import Balok
+from balok_controller import BalokController
+
+class BalokView:
+
+    def show_luas(self, balok: Balok, balok_controller: BalokController):
+        print (balok_controller.hitung_luas(balok))
+        
+    def show_keliling(self, balok: Balok, balok_controller: BalokController):
+        print (balok_controller.hitung_keliling(balok))
